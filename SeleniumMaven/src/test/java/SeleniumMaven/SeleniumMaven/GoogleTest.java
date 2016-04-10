a773ee3e25082fe8031433b461e08b76a7b02f26
Ram <mrdram28@gmail.com>
2016-04-11 01:07:57 +0530

    Modified Google Test

--- SeleniumMaven/src/test/java/SeleniumMaven/SeleniumMaven/GoogleTest.java
+++ SeleniumMaven/src/test/java/SeleniumMaven/SeleniumMaven/GoogleTest.java
@@ -2,6 +2,7 @@ package SeleniumMaven.SeleniumMaven;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -23,8 +24,23 @@ public class GoogleTest {
 	} 
 
 	@Test 
-	public void testToCompareDoubles() { 
-	driver.get("http://www.google.com");
+	public void testToCompareDoubles() {
+		
+		System.out.println("#####################################");
+    	
+    	System.out.println("Starting Firefox Driver");
+    	
+        driver.get("http://www.guru99.com/selenium-tutorial.html"); 
+        
+        System.out.println("Opened Guru Url");
+        
+        String title = driver.getTitle();   
+        
+        System.out.println("Title is : "+title);
+        
+        Assert.assertTrue(title.contains("Free Selenium Tutorials"));
+        
+        System.out.println("#####################################");
 	 } 
 	
 }
